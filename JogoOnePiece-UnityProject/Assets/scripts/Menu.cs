de718b65a8a8a4987017a6affe040e3aeef91f5a
Moises1605 <56726033+Moises1605@users.noreply.github.com>
2020-09-29 13:26:37 -0300

    protótipo de tela inicial

---
+++ JogoOnePiece-UnityProject/Assets/scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+ 
+    // Update is called once per frame 
+    void Update()
+    {
+        
+    } 
+
+    public void CarregarJogo(){
+		SceneManager.LoadScene ("Jogo");
+    }  
+
+    public void CarregarAbout(){
+		SceneManager.LoadScene ("about");
+	}  
+
+    public void CarregarInicial(){
+		    SceneManager.LoadScene ("inicial");
+	} 
+}

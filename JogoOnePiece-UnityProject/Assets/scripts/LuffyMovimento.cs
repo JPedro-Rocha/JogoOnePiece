7fdc3e92c87e8f7f04be2c075fa307e18945fe96
JPedro-Rocha <jp.oliveirarocha@hotmail.com>
2020-09-30 23:59:52 -0300

    começo movimento personagem

---
+++ JogoOnePiece-UnityProject/Assets/scripts/LuffyMovimento.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuffyMovimento : MonoBehaviour
+{
+    public LuffyController controller;
+    public float runSpeed = 40f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(Input.GetAxisRaw("Horizontal") * runSpeed * Time.fixedDeltaTime, false, false);
+    }
+}
+

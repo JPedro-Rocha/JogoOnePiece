50ab5b36310674ad9ee4a8fefddd9387ad7a7eae
JPedro-Rocha <jp.oliveirarocha@hotmail.com>
2020-10-01 18:40:30 -0300

    movimento e animação update

--- JogoOnePiece-UnityProject/Assets/scripts/LuffyMovimento.cs
+++ JogoOnePiece-UnityProject/Assets/scripts/LuffyMovimento.cs
@@ -6,16 +6,25 @@ public class LuffyMovimento : MonoBehaviour
 {
     public LuffyController controller;
     public float runSpeed = 40f;
-
+    public bool jump;
+    public Animator animator;
     // Update is called once per frame
     void Update()
     {
+        animator.SetFloat("Speed", Mathf.Abs(Input.GetAxisRaw("Horizontal") * runSpeed));
+        animator.SetBool("isJumping", jump);
 
+        if (Input.GetButton("Jump"))//tecla 'w'
+        {
+            jump = true;
+        }
     }
 
     void FixedUpdate()
     {
-        controller.Move(Input.GetAxisRaw("Horizontal") * runSpeed * Time.fixedDeltaTime, false, false);
+        controller.Move(Input.GetAxisRaw("Horizontal") * runSpeed * Time.fixedDeltaTime, false, jump);
+        jump = false;
+
     }
 }
 
